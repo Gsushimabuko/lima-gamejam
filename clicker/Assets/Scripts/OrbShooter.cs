ea8a82345dad33e393b477e9a7e83b28fafb546a
Gabriel Shimabuko <shimabukogabriel@gmail.com>
2025-01-26 14:49:35 -0500

    Revert "gabooChanges"
    This reverts commit 9cd7348a2e4a1596570bb3e2133661416eb8a8fe.

--- clicker/Assets/Scripts/OrbShooter.cs
+++ clicker/Assets/Scripts/OrbShooter.cs
@@ -51,7 +51,6 @@ public class OrbShooter : MonoBehaviour
     {
         if (bulletPrefab == null || firePoint == null) return;
 
-        // PLAY SOUND FIRE
         GameObject bullet = projectilesPool.AskForProjectile(bulletPrefab.name, firePoint.position);
         Vector3 direction = (target - firePoint.position).normalized;
 
