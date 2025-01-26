f1f8ce7f98017039dff7495fc40d6372281e317d
JAVHM <javanhmalpro@gmail.com>
2025-01-26 08:20:31 -0500

    assets graphs linear fggraph added

--- clicker/Assets/Scripts/LinearGraph/LinearGraphController.cs
+++ clicker/Assets/Scripts/LinearGraph/LinearGraphController.cs
@@ -4,19 +4,29 @@ using UnityEngine;
 
 public class LinearGraphController : MonoBehaviour
 {
-    private Vector3 initialPosition = new Vector3(-14.5699997f, -0.170000002f, 0);
+    private Vector3 initialPosition = new Vector3(-63.6599998f, 0, 0);
+
+    private Vector3 repeatPosition = new Vector3(-15.5100002f, 0, 0);
+
+    private float speed = 5;
 
     //--------------------------------------------------------------------------------
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        transform.localPosition = initialPosition;
     }
 
     // Update is called once per frame
     void Update()
     {
+        transform.localPosition = transform.localPosition + (Vector3.right * speed * Time.deltaTime); 
+        
+        if (transform.localPosition.x >= repeatPosition.x)
+        {
+            transform.localPosition = initialPosition;
+        }
         
     }
 }
